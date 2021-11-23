16fb25f875085597a131e52bc19b5fa17d906f28
solidsign <49648821+solidsign@users.noreply.github.com>
2021-11-23 10:20:49 +0300

    Added MovementController.cs

---
+++ Assets/Scripts/Movement/MovementController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class MovementController : MonoBehaviour
+    {
+        private CharacterController _character;
+        private Vector3 _displacement;
+
+        private void Awake()
+        {
+            _character = GetComponent<CharacterController>();
+        }
+
+        private void LateUpdate()
+        {
+            _character.Move(_displacement);
+            _displacement = Vector3.zero;
+        }
+
+        public void Move(Vector3 displacement)
+        {
+            _displacement += displacement;
+        }
+    }
+}
